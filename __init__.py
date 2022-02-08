1983a5adf0a69f87288c236741fef5e815a3c24f
techpluscraft <techpluscraft@gmail.com>
2022-02-08 16:50:23 -0500

    added run

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from bot.__main__ import run
+
+run()
--- bot/__main__.py
+++ bot/__main__.py
@@ -3,15 +3,17 @@ from nextcord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+        
+async def run():
+    client = commands.Bot(command_prefix="/")
 
-client = commands.Bot(command_prefix="/")
+    @client.event
+    async def on_ready():
+        print("Bot is ready!")
+        
+    for filename in os.listdir('./bot/cogs'):
+        if filename.endswith('.py'):
+            client.load_extension(f'cogs.{filename[:-3]}')
+    client.run(os.getenv('TOKEN'))
 
-@client.event
-async def on_ready():
-    print("Bot is ready!")
-    
-for filename in os.listdir('./bot/cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
-
-client.run(os.getenv('TOKEN'))
+run()
