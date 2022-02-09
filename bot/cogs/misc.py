c0952da4be6b95a516511cecc98276454c321689
techpluscraft <techpluscraft@gmail.com>
2022-02-08 21:09:19 -0500

    Added Docstrings and descriptions for slash commands

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -6,20 +6,24 @@ class misc(commands.Cog, name="Misc"):
     def __init__(self, client):
         self.client = client
     
-    @nextcord.slash_command()
+    @nextcord.slash_command(name="ping",description="Pong!")
     async def ping(self,interaction: nextcord.Interaction):
+        """Pong!"""
         await interaction.response.send_message("Pong!")
 
-    @nextcord.slash_command()
+    @nextcord.slash_command(name="echo",description="Echos the message.")
     async def echo(self,interaction: nextcord.Interaction):
+        """Echos the message."""
         await interaction.response.send_message(interaction.message.content)
     
-    @nextcord.slash_command()
+    @nextcord.slash_command(name="help",description="Shows help.")
     async def help(self,interaction: nextcord.Interaction):
-        await interaction.response.send_message("```\n/ping - Pong!\n/echo - Echo the message\n/help - This message\n```")
+        """Shows help."""
+        await interaction.response.send_message("```\n/ping - Pong!\n/echo - Echo the message\n/help - This message\n/source - Source Code\n```")
     
-    @nextcord.slash_command()
+    @nextcord.slash_command(name="source",description="Shows the source code.")
     async def source(self,interaction: nextcord.Interaction):
+        """Shows the source code."""
         await interaction.response.send_message("https://github.com/Techstriker-Solutions/blue-bot")
 
 def setup(client):
